99af76f22fb3cdfb7bc1542de88634833ebdd448
santi <santiagodelafuente2013@gmail.com>
2022-05-30 15:49:55 +0200

    add controller weeks

---
+++ app/controllers/weeks_controller.rb
@@ -0,0 +1,7 @@
+class WeeksController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
---
+++ test/controllers/weeks_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class WeeksControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get weeks_show_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get weeks_index_url
+    assert_response :success
+  end
+
+end
