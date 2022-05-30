2dc4cff7e2d3ca7afb7b87b47df18afdd442eaf9
santi <santiagodelafuente2013@gmail.com>
2022-05-30 16:10:58 +0200

    add seed and CVS (to lib/seeds)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'merchants.csv'))
+csv = CSV.parse(csv_text.scrub, headers: true)
+csv.each do |row|
+  Merchant.create(name: row['name'], email: row['email'], cif: row['cif'])
+end
+puts "Merchants upload finished"
+
+#--------------------------------------------------------------------
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'shoppers.csv'))
+csv = CSV.parse(csv_text.scrub, headers: true)
+csv.each do |row|
+  Shopper.create(name: row['name'], email: row['email'], nif: row['nif'])
+end
+puts "Shoppers upload finished"
+
+#--------------------------------------------------------------------
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'orders.csv'))
+csv = CSV.parse(csv_text.scrub, headers: true)
+csv.each do |row|
+  Order.create(merchant_id: row['merchant_id'], shopper_id: row['shopper_id'], amount: row['amount'], created_at: row['created_at'], completed_at: row['completed_at'], week: row['week'])
+end
+puts "Orders upload finished"
