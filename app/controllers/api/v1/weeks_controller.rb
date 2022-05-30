d4184de5adeb867b5c232f038a79f565adcf8e40
santi <santiagodelafuente2013@gmail.com>
2022-05-30 16:03:30 +0200

    add show in controller weeks

---
+++ app/controllers/api/v1/weeks_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::WeeksController < ApplicationController
+
+  def show
+    @merchants = Merchant.all
+    @desembolsos = Order.where('week = ?', params[:id]) or not_found
+    if @desembolsos.count != 0
+      @week_desembolsos = @desembolsos.where.not(completed_at: nil)
+                                      .where('week = ?', @desembolsos.first.week)
+    end
+
+    @informe = SayHelloJob.set(wait: 1.minutes).perform_later(params[:id])
+  end
+end
--- app/controllers/weeks_controller.rb
+++
@@ -1,7 +0,0 @@
-class WeeksController < ApplicationController
-  def show
-  end
-
-  def index
-  end
-end
